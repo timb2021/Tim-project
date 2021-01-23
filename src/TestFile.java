9395a90da37d9b516a87306aeac65dfba26ee2b4
System Administrator <timofeibalaban@gmail.com>
2021-01-23 15:02:18 -0500

    Test file

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,2 +1,8 @@
 public class TestFile {
+    public static void main(String[] args) {
+        String a = "first";
+        String b = "second";
+
+        System.out.println(a + " " + b);
+    }
 }
